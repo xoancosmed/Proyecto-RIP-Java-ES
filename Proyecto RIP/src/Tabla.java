9c33379e23da400a988835462e43b52b56101dd1
Xoán <xoancosmed@gmail.com>
2016-05-06 21:28:52 +0200

    Reorganización. Eliminado ElementoTabla.java (ahora es subclase).

--- Proyecto RIP/src/Tabla.java
+++ Proyecto RIP/src/Tabla.java
@@ -49,5 +49,64 @@ public class Tabla {
 		return tabla.get(subred);
 		
 	}
+	
+	public class ElementoTabla {
+
+		private String subred;
+		private String mascara;
+		private int g;
+		private String vecino;
+		private int coste;
+		
+		public ElementoTabla(String subred, String mascara, int g, String vecino, int coste) {
+			super();
+			this.subred = subred;
+			this.mascara = mascara;
+			this.g = g;
+			this.vecino = vecino;
+			this.coste = coste;
+		}
+
+		public String getSubred() {
+			return subred;
+		}
+
+		public void setSubred(String subred) {
+			this.subred = subred;
+		}
+
+		public String getMascara() {
+			return mascara;
+		}
+
+		public void setMascara(String mascara) {
+			this.mascara = mascara;
+		}
+
+		public int getG() {
+			return g;
+		}
+
+		public void setG(int g) {
+			this.g = g;
+		}
+
+		public String getVecino() {
+			return vecino;
+		}
+
+		public void setVecino(String vecino) {
+			this.vecino = vecino;
+		}
+
+		public int getCoste() {
+			return coste;
+		}
+
+		public void setCoste(int coste) {
+			this.coste = coste;
+		}
+		
+	}
 
 }
