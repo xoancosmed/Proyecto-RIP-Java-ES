2e39ad0a86667a3ea0f2e3c7f536ad5f03bb7d44
redord9 <redord9@pc-ip.143>
2016-05-19 15:54:44 +0200

    hola hise un if , nunca mas de 16

--- Proyecto RIP/src/Rip.java
+++ Proyecto RIP/src/Rip.java
@@ -442,13 +442,16 @@ public class Rip {
 				}
 				
 				Paquete.RIPv2[] ripRecibido = Paquete.obtenerEntradas(recData);
+				int coste=16;
 				
 				for (int k = 0; k < ripRecibido.length; k++) {
 					
 					String subred = ripRecibido[k].getIp();
 					String mascara = ripRecibido[k].getMascara();
 					Router vecino = new Router(datagramPacket.getAddress().getHostAddress(), datagramPacket.getPort());
-					int coste = ripRecibido[k].getCoste() + 1;
+					if(ripRecibido[k].getCoste()!=16){
+						coste = ripRecibido[k].getCoste() + 1;
+					}
 					int g = obtenerG(subred,vecino,coste);
 					
 					if(subred.equalsIgnoreCase(ip)) continue;
